3dce3f6bc94fe0245fd88d01428c22b7c8db62e3
Sakshi Mishra <2003sakshimishra@gmail.com>
2026-03-31 20:59:16 +0530

    Day 10 – Added API integration in frontend using Axios

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 30000,
 });
 
-export const fetchProfile = (username) => {
-  return API.get(`/profile/${username}`);
+export const fetchProfile = async (username) => {
+  const { data } = await api.get(`/profile/${username}`);
+  return data;
 };
+
+export const fetchCachedProfile = async (username) => {
+  const { data } = await api.get(`/profile/${username}/cached`);
+  return data;
+};
+
+export const compareProfiles = async (u1, u2) => {
+  const { data } = await api.get(`/compare?u1=${u1}&u2=${u2}`);
+  return data;
+};
+
+export default api;
